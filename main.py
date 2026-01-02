c67303ab30d8e7c712a279dbe1623363cf9d6c77
Fatemeh Feyzipour <fatemehfeizipur@gmail.com>
2026-01-02 04:58:43 -0800

    Add interactive command-line tool

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Fatemeh Your python project is working!")
+name = input("what is your name? ")
+age = int(input("How old are you?"))
+
+print(f"\nHello {name} ")
+print(f"Next year, you will be {age + 1 } years old.")
+
+if age >=18:
+	print("You are an adult")
+else:
+	print("You are under 18")
